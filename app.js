22d1017695d0e54f7d3c4a9d2baaafe7fece6274
Chiemezuo <chiemezuoakujobi@gmail.com>
2023-05-23 23:31:20 +0100

    Create default page

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const app = express()
 app.use('/api/posts', postRouter)
 app.use('/api/comments', commentsRouter)
 
+app.get('/', (req, res) => {
+  res.send('Welcome aboard, please modify the URL as stated in the README.md file')
+})
+
 app.get('*', (req, res) => {
   res.send('Oops, I think you mistyped your URL. Double check what you typed.')
 })
